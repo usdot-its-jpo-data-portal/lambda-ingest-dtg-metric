e3fad76345e0c3042291c2765aff12efa67b2c60
chuehlien <lien_julia@bah.com>
2021-02-11 16:43:56 -0500

    Rename repo name

--- package.sh
+++ package.sh
@@ -2,12 +2,12 @@
 
 # NOTE!
 # This script is deprecated and was intended for manual deployment packaging.
-rm lambda-ingest-dtg-metrics.zip
+rm lambda-ingest-dtg-metric.zip
 mkdir _package
 pip install -r requirements.txt --upgrade --target _package
 cp src/* _package/
 cd _package/
-zip -r ../lambda-ingest-dtg-metrics.zip *
+zip -r ../lambda-ingest-dtg-metric.zip *
 cd ..
 rm -rf _package/
-echo "Created package in lambda-ingest-dtg-metrics.zip"
+echo "Created package in lambda-ingest-dtg-metric.zip"
