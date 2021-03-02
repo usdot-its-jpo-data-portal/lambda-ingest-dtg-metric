c313dba7a7df1573ebff2202fb2ad09b4dda72cd
chuehlien <lien_julia@bah.com>
2021-03-02 12:43:48 -0500

    Modify the lambda file to be also used as a script

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -13,6 +13,7 @@ from datahub_metrics_ingest.util import write_metrics_to_csv
 USER_ID = '9k5r-tgy7'
 ELASTICSEARCH_API_BASE_URL = os.environ.get('ELASTICSEARCH_API_BASE_URL')\
     if os.environ.get('ELASTICSEARCH_API_BASE_URL') is not None else 'http://localhost'
+LIMIT_N = 50000
 
 
 def lambda_handler(event, context):
@@ -21,17 +22,29 @@ def lambda_handler(event, context):
     sdate = edate - timedelta(hours=24)
     ingest(sdate, edate)
 
-def ingest(sdate: datetime, edate: datetime, write_to_es: bool = True):
+def ingest(sdate: datetime, edate: datetime, write_to_es: bool = True, elasticsearch_host: str = ELASTICSEARCH_API_BASE_URL):
+    if write_to_es:
+        print(f'Ingesting data from {sdate.isoformat()[:10]} - {edate.isoformat()[:10]} into metrics index at {elasticsearch_host}')
+    else:
+        print(f'Ingesting data from {sdate.isoformat()[:10]} - {edate.isoformat()[:10]} to CSV file')
     try:
         asset_ids = get_data_asset_ids()
-        all_metrics = get_metrics(sdate, edate)
-        filtered_metrics = [i for i in all_metrics if i['asset_uid'] in asset_ids]
-
-        formatter = FormatterFactory().get_formatter('socrata')
-        metric_objs = formatter.get_data_objects(filtered_metrics)
-        if write_to_es:
-            ElasticsearchDAO(ELASTICSEARCH_API_BASE_URL).write_to_elasticsearch(metric_objs)
-        return metric_objs
+        done = False
+        offset = 0
+        all_metric_objs = []
+        while not done:
+            all_metrics = get_metrics(sdate, edate, offset)
+            offset += LIMIT_N
+            if len(all_metrics) < LIMIT_N:
+                done = True
+            filtered_metrics = [i for i in all_metrics if i['asset_uid'] in asset_ids]
+            formatter = FormatterFactory().get_formatter('socrata')
+            metric_objs = formatter.get_data_objects(filtered_metrics)
+            if write_to_es:
+                ElasticsearchDAO(elasticsearch_host).write_to_elasticsearch(metric_objs)
+            else:
+                all_metric_objs += metric_objs
+        return all_metric_objs
     except Exception as e:
         msg = f"Error ingesting SCGC metrics for {sdate.isoformat()[:10]} ==> {str(e)}"
         print(msg)
@@ -43,17 +56,35 @@ def get_data_asset_ids():
     asset_ids = [i['resource']['id'] for i in catalog]
     return asset_ids
 
-def get_metrics(sdate: datetime, edate: datetime):
+def get_metrics(sdate: datetime, edate: datetime, offset: int = 0):
     where_clause = f"timestamp >= '{sdate.strftime('%Y-%m-%d')}T00:00:00.000' AND timestamp < '{edate.strftime('%Y-%m-%d')}T00:00:00.000'"
-    all_metrics = requests.get(
+    metrics = requests.get(
         'https://datahub.transportation.gov/resource/fa6d-d2xr.json',
-        params={'$where': where_clause, '$limit': 50000}
+        params={'$where': where_clause, '$limit': LIMIT_N, '$offset': offset}
     ).json()
-    return all_metrics
+    return metrics
 
 
 if (__name__ == '__main__'):
-    edate = datetime.today()
-    sdate = edate - timedelta(hours=24)
-    metric_objs = ingest(sdate, edate, write_to_es=False)
-    write_metrics_to_csv(f"dtg_metrics_{sdate.strftime('%Y-%m-%d')}.csv", metric_objs)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ingest_historical", dest='ingest_historical', action='store_true', default=False,
+        help="Ingest all daily DTG metrics from 2017-11-01 until yesterday. Otherwise, will ingest only metrics for yesterday.")
+    parser.add_argument("--write_to_es", dest='write_to_es', action='store_true', default=False,
+        help="Write metrics Elasticsearch. Otherwise, will write metrics to CSV.")
+    parser.add_argument("--es_host", dest='es_host', default="http://localhost",
+        help="Elasticsearch host to write the metrics to. Default: http://localhost.")
+    
+    args = parser.parse_args()
+    
+    if args.ingest_historical:
+        edate = datetime.today() - timedelta(hours=24)
+        sdate = datetime(2017,11,1)
+    else:
+        edate = datetime.today()
+        sdate = edate - timedelta(hours=24)
+    
+    metric_objs = ingest(sdate, edate, write_to_es=args.write_to_es, elasticsearch_host=args.es_host)
+    if not args.write_to_es:
+        fp = f"dtg_metrics_{sdate.strftime('%Y-%m-%d')}.csv"
+        write_metrics_to_csv(fp, metric_objs)
+        print(f'Wrote {len(metric_objs)} records to {fp}')
